a67c92b1aac5602c7190b2f98e8e5a70407db75d
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-03-03 19:15:17 -0300

    Imc Completo, falta Refatorar e quero fazer uns complementos como armazenar em um banco de dados as pessoas que mediram seu IMC/BMI

--- src/bmi.js
+++ src/bmi.js
@@ -10,6 +10,21 @@ function handleBMI() {
 
     const bmi = calculateBMI(weight, height);
     console.log(`Your BMI is ${bmi.toFixed(2)}`);
+
+    if (bmi < 18.5)  {
+        console.log('You are underweight');
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        console.log('You are normal weight');
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        console.log('You are overweight');
+    } else if ( bmi >= 30 && bmi <= 34.9) {
+        console.log('You are class I obese');
+    } else if (bmi >= 35 && bmi <= 39.9) {
+        console.log('You are class II obese');
+    } else {
+        console.log('You are obese class III and IV');
+    }
 }
 
 handleBMI();
+
