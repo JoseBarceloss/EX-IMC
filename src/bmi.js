60bb4c6abac9f2124d7f86f098616b8debdd4d84
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-03-03 22:15:32 -0300

    ficou show

--- src/bmi.js
+++ src/bmi.js
@@ -1,30 +1,75 @@
+const fs = require('fs').promises;
+const readlineSync = require('readline-sync');
+
 function calculateBMI(weight, height) {
     return weight / (height ** 2);
 }
 
-const readlineSync = require('readline-sync');
+async function handleBMI() {
 
-function handleBMI() {
+    const name = readlineSync.question('What\'s your name? ');
     const weight = readlineSync.questionFloat('Enter your weight in kg: ');
     const height = readlineSync.questionFloat('Enter your height in meters: ');
 
     const bmi = calculateBMI(weight, height);
-    console.log(`Your BMI is ${bmi.toFixed(2)}`);
-
+    let situation;
     if (bmi < 18.5)  {
-        console.log('You are underweight');
+        situation = 'Underweight';
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-        console.log('You are normal weight');
+        situation = 'Normal weight';
     } else if (bmi >= 25 && bmi <= 29.9) {
-        console.log('You are overweight');
+        situation = 'Overweight';
     } else if ( bmi >= 30 && bmi <= 34.9) {
-        console.log('You are class I obese');
+        situation = 'Class I obese';
     } else if (bmi >= 35 && bmi <= 39.9) {
-        console.log('You are class II obese');
+        situation = 'Class II obese';
     } else {
-        console.log('You are obese class III and IV');
+        situation = 'Obese class III and IV';
+    }
+
+    console.log(`Your BMI is ${bmi.toFixed(2)}`);
+    console.log(`You are ${situation}`);
+
+    const id = new Date().toISOString();
+
+    let jsonData;
+    try {
+        const data = await fs.readFile('src/data/data.json', 'utf8');
+        if (data.trim() === '') {
+            jsonData = [];
+        } else {
+            jsonData = JSON.parse(data);
+            if (!Array.isArray(jsonData)) {
+                throw new Error("Data in file is not an array.");
+            }
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            jsonData = [];
+            await fs.writeFile('src/data/data.json', '[]');
+        } else {
+            console.error("Error reading file:", err);
+            return;
+        }
+    }
+
+    const newData = {
+        id: id,
+        name: name,
+        weight: weight,
+        height: height,
+        bmi: bmi.toFixed(2),
+        situation: situation
+    };
+
+    jsonData.push(newData);
+
+    try {
+        await fs.writeFile('src/data/data.json', JSON.stringify(jsonData, null, 2));
+        console.log('Data has been saved!');
+    } catch (err) {
+        console.error("Error writing file:", err);
     }
 }
 
 handleBMI();
-
